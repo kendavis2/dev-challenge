a3dcab9be851f091d4a8838a039f2d38f9801c86
Kenneth Davis <kenneth.davis2@turner.com>
2016-07-28 13:51:43 -0700

    added mongo db connection

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
 'use strict';
 
 const Hapi = require('hapi');
+const MongoClient = require('mongodb').MongoClient;
+const Promise = require("bluebird");
 
-console.log("trying...");
+const assert = require('assert');
 
-// Create a server with a host and port
+const url = 'mongodb://readonly:turner@ds043348.mongolab.com:43348/dev-challenge';
+
+// server config
 const server = new Hapi.Server();
 server.connection({ 
     port: process.env.PORT || 5000 
 });
 
-// Add the route
+// routes
 server.route({
     method: 'GET',
     path:'/hello', 
     handler: function (request, reply) {
-
         return reply('hello world!');
     }
 });
 
-// Start the server
-server.start((err) => {
-    console.log("still trying...");
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected to database.");
+
+    process.on('exit', exitHandler.bind(null,{cleanup:true, db}));
+    process.on('SIGINT', exitHandler.bind(null, {exit:true, db}));
+    process.on('uncaughtException', exitHandler.bind(null, {exit:true, db}));
+
+    server.start((err) => {
+        if (err) {
+            throw err;
+        }
+        console.log('Server running at:', server.info.uri);
+    });
 });
+
+function exitHandler(options, err) {
+    console.log("Cleanup...");
+    options.db.close();
+}
+
+
